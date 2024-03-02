83e234ae7e2301f51c4f50424c75ef93a565ac08
Ritika-d27 <109162000+Ritika-d27@users.noreply.github.com>
2024-03-02 23:32:02 +0530

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,38 +1,67 @@
 import mongoose from "mongoose";
 
-const OrderSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user',
+const orderSchema = new mongoose.Schema(
+  {
+    vegetable: {
+      type: String,
+      required: [true, "Please add vegetable name"],
+      trim: true,
     },
-
+    quantity_type: {
+      type: String,
+      enum: ["quintal", "kg"],
+      default: "kg",
+      required: [true, "Please select quantity type"],
+    },
+    quantity: {
+      type: String,
+      required: [true, "Please select quantity"],
+    },
+    quality: {
+      type: String,
+      required: [true, "Please specify quality"],
+    },
+    price: {
+      type: String,
+      required: [true, "Please add price"],
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
     product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'product',
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "product",
     },
 
     address: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
 
     phoneNo: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
 
     transportationRequired: {
-        type: Boolean,
-        required: true,
+      type: Boolean,
+      required: true,
     },
 
     paymentMode: {
-        type: String,
-        enum: ['online', 'offline'],
-        default: 'online',
-    }
+      type: String,
+      enum: ["online", "offline"],
+      default: "online",
+    },
 
-}, { timestamps: true });
+    role: {
+      type: String,
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
 
-const Order = mongoose.model('order', OrderSchema);
+const Order = mongoose.model("order", orderSchema);
 export default Order;
