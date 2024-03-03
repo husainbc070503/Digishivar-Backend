3e2c9f3de796556e57bf9e95982c1d127db735bf
Husain Champawala <husainchampawala@gmail.com>
2024-03-03 16:44:42 +0530

    small update

--- models/userProduct.js
+++ models/userProduct.js
@@ -36,7 +36,7 @@ const ProductSchema = new mongoose.Schema(
     },
     user: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      ref: "user",
     },
   },
   { timestamps: true }
