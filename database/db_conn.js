ee776bc0e481015cb9b2e87ee7d5478625f47e3c
Husain Champawala <husainchampawala@gmail.com>
2024-03-04 11:53:13 +0530

    added url

--- database/db_conn.js
+++ database/db_conn.js
@@ -7,7 +7,7 @@ const url = process.env.MONGODB_URL;
 const connectToDB = async () => {
     mongoose.set('strictQuery', false);
     try {
-        await mongoose.connect(url, () => console.log('Connected to mongodb compass'));
+        await mongoose.connect(url, () => console.log('Connected to mongodb atlas'));
     } catch (error) {
         console.log(error.message);
     }
