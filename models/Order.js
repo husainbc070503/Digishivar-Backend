d8d6b86bbf3034bf0053a26f2ca395814639704a
Husain Champawala <husainchampawala@gmail.com>
2024-03-12 05:44:19 +0530

    updated

--- models/Order.js
+++ models/Order.js
@@ -6,6 +6,10 @@ const orderSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "user",
     },
+    farmer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+    },
     products: [
       {
         pro: {
--- routes/Order.js
+++ routes/Order.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import Order from "../models/Order.js";
 import ValidateUser from "../middlewares/ValidateUser.js";
 import isCustomer from "../middlewares/isCustomer.js";
+import isFarmer from "../middlewares/isFarmer.js";
 const router = Router();
 
 router.post("/placeOrder", ValidateUser, isCustomer, async (req, res) => {
@@ -9,9 +10,11 @@ router.post("/placeOrder", ValidateUser, isCustomer, async (req, res) => {
     var order = await Order.create({ ...req.body, user: req.user });
     order = await Order.findById(order._id)
       .populate("user", "-password")
+      .populate('farmer')
       .populate("products.pro");
 
     res.status(200).json({ success: true, order });
+
   } catch (error) {
     res.status(400).json({ success: false, message: error.message });
   }
@@ -21,11 +24,28 @@ router.get("/orders", ValidateUser, async (req, res) => {
   try {
     const orders = await Order.find()
       .populate("user", "-password")
+      .populate('farmer')
       .populate("products.pro");
+
     res.status(200).json({ success: true, orders });
+
   } catch (error) {
     res.status(400).json({ success: false, message: error.message });
   }
 });
+
+router.put('/changePaymentStatus/:id', ValidateUser, isFarmer, async (req, res) => {
+  try {
+    const order = await Order.findByIdAndUpdate(req.params.id, { paymentStatus: true }, { new: true })
+      .populate("user", "-password")
+      .populate('farmer')
+      .populate("products.pro");
+
+    res.status(200).json({ success: true, order });
+
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message })
+  }
+})
 
 export default router;
